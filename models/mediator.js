419f8be3d67b613b6c287d7a946714413cdd2497
Lamiaa Hassan <mkabary8@gmail.com>
2023-05-02 13:33:14 +0200

    mediatorSchema

---
+++ models/mediator.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const mediatorSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  phoneNumber: { type: String, required: true }
+  
+});
+
+module.exports = mongoose.model('Mediator', mediatorSchema);
