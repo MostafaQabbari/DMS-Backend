c995e07fdb3f47189949e27eef9021a7b692549a
Abdo Samir <abdo.samir.7719@gmail.com>
2023-05-25 16:27:33 +0300

    zxzxc

--- routes/clinet1data.js
+++ routes/clinet1data.js
@@ -60,8 +60,8 @@ router.patch("/addClient1/:id", async (req, res) => {
         let currentCase = await Case.findById(req.params.id);
         console.log("xxx")
         let client1data = req.body
-        let Reference = `${req.body.personalInfo.surName}& ${req.body.Client2Details.SurName}`;
         console.log(client1data)
+        let Reference = `${req.body.personalInfo.surName}& ${req.body.Client2Details.SurName}`;
         console.log(Reference)
 
         await createMIAM1Upload(client1data , Reference);
