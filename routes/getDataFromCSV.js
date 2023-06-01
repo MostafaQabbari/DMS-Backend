b4622e54f7b6f5a4dbe125b565091ba2a6134c57
Abdo Samir <abdo.samir.7719@gmail.com>
2023-06-01 17:17:16 +0300

    xyzz

--- routes/getDataFromCSV.js
+++ routes/getDataFromCSV.js
@@ -3,11 +3,11 @@ const csv = require('csv-parser');
 const fs = require('fs');
 const express = require('express');
 const router = express.Router();
-const multer = require('multer')
-const Case = require('../models/case');
-const mediator = require('../models/mediator');
+const CSVdata = require('../interface/CSVdata')
 
-const CSVdata  = require('../interface/CSVdata')
+
+const Airtable = require('airtable');
+
 
 router.post('/getDataFromCSV', (req, res) => {
   const filePath = 'uploads/csvToTest.csv'; // Update with the path to your CSV file
@@ -21,7 +21,7 @@ router.post('/getDataFromCSV', (req, res) => {
 
       if (results.length < 1) {
         // console.log(data)
-       const x = CSVdata(data)
+        const x = CSVdata(data)
         results.push(x)
       }
 
@@ -40,6 +40,64 @@ router.post('/getDataFromCSV', (req, res) => {
 
 });
 
+router.post('/getDataFromAirtable', (req, res) => {
+
+  /**
+ * apiKey: 'keySEWVOJ1gOsDSSP'
+ * base : 'efQFIQirYoTu5B' 
+ * tableID = 'tblkEfKYHH6sjHiNK';
+ */
+
+  const base = new Airtable({ apiKey: 'keySEWVOJ1gOsDSSP' }).base('appefQFIQirYoTu5B');
+
+  const tableName = 'tblkEfKYHH6sjHiNK';
+
+  const view1Records = [];
+  const view2Records = [];
+
+  base(tableName)
+    .select({
+      view: 'viwLuC08eiToGUL3I', // 'VIEW1_ID' 
+      maxRecords: 20
+    })
+    .eachPage((records, fetchNextPage) => {
+
+      records.forEach(record => {
+        view1Records.push(record);
+      });
+
+      fetchNextPage();
+    }, err => {
+      if (err) {
+        console.error('Error:', err);
+        return;
+      }
+      
+
+      // console.log(view1Records[0].fields['RecordID (from Cases (C1 Link))'])
+      console.log('View 1 records fetched successfully.');
+      // get C2 record
+      base(tableName).find(view1Records[0].fields['RecordID (from Cases (C1 Link))'], (err, record) => {
+        if (err) {
+          console.error('Error:', err);
+          return;
+        }
+      
+        // Record C2 retrieved successfully
+       
+        console.log('Retrieved record:', record.fields);
+      });
+
+      res.json(view1Records[0].fields)
+    });
+
+
+
+
+
+
+});
+
 
 
 
@@ -48,39 +106,4 @@ module.exports = router
 
 
 
-
-
-
-/*
-    const file = req.files.file; // Assuming you are using a file upload library such as 'multer'
-  
-    fs.createReadStream(file.tempFilePath)
-      .pipe(csv())
-      .on('data', (data) => {
-        // Process each row of the CSV data and insert it into MongoDB
-        MongoClient.connect(mongoURL, (err, client) => {
-          if (err) {
-            console.error('Error connecting to MongoDB:', err);
-            return;
-          }
-  
-          const db = client.db(dbName);
-          const collection = db.collection('your-collection-name'); // Update with your collection name
-  
-          // Insert the data into the collection
-          collection.insertOne(data, (err, result) => {
-            if (err) {
-              console.error('Error inserting data:', err);
-            }
-          });
-  
-          client.close();
-        });
-      })
-      .on('end', () => {
-        res.send('Data imported successfully');
-      });
-  });
-  
-   */
 
