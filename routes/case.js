1ab2f7234215fd6b3ca1fb6eeed85a43b369306a
Abdo Samir <abdo.samir.7719@gmail.com>
2023-08-14 16:09:09 +0300

    add case closed in case list

--- routes/case.js
+++ routes/case.js
@@ -105,10 +105,11 @@ const sendMailPassporting = function (companyData, clientData, messageBodyinfo) 
     to: clientData.email,
     subject: `Applying To ${messageBodyinfo.formType} Form`,
     html: ` <div style="background-color: #72A0C1 ; text-align: center; padding: 5vw; width: 75%; margin: auto;">
-     <h1>Dear ${clientData.clientName}  </h1>
-    <p> Thanks for booking you MIAM booking you MIAM  , here is your passporting form </p>
-     <p> Please click on the link below :</p>
+     <h1>Hi  ${clientData.clientName}  </h1>
+    <p> Thank you for contacting us regarding your Legal Aid application for family mediation.
+    To start your application please follow the link below:</p>
     <a href='${messageBodyinfo.formUrl}'  style="color:white; padding:5px; "> ${messageBodyinfo.formUrl} </a>
+    <p>Applications are only considered via this route.</p>
     <h3>Direct Mediation Services</h3>
     <h4>${companyData.companyName}</h4>
     <h4>${companyData.email}</h4>
@@ -164,9 +165,11 @@ const sendMailLowIncome = function (companyData, clientData, messageBodyinfo) {
     subject: `Applying To ${messageBodyinfo.formType} Form`,
     html: ` <div style="background-color: #72A0C1 ; text-align: center; padding: 5vw; width: 75%; margin: auto;">
      <h1>Dear ${clientData.clientName}  </h1>
-    <p> Thanks for booking you MIAM , here is your low income / no income form </p>
-     <p> Please click on the link below :</p>
+    <p>Thank you for contacting us regarding your Legal Aid application for family mediation.
+    To start your application please follow the link below::</p>
+     
     <a href='${messageBodyinfo.formUrl}'  style="color:white; padding:5px; "> ${messageBodyinfo.formUrl} </a>
+    <h3>Applications are only considered via this route .</h3>
     <h3>Direct Mediation Services</h3>
     <h4>${companyData.companyName}</h4>
     <h4>${companyData.email}</h4>
@@ -383,6 +386,7 @@ router.get('/getCasesList', authMiddleware, async (req, res) => {
           Reference: cases.cases[i].Reference,
           status: cases.cases[i].status,
           startDate: cases.cases[i].startDate,
+          closed:cases.cases[i].closed
         }
 
         casesList.push(resposedCaseObj)
@@ -405,6 +409,7 @@ router.get('/getCasesList', authMiddleware, async (req, res) => {
           Reference: cases.cases[i].Reference,
           status: cases.cases[i].status,
           startDate: cases.cases[i].startDate,
+          closed:cases.cases[i].closed
         }
 
         casesList.push(resposedCaseObj)
