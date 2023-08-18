3a4ce143a69913e568e23f7ea2cf1661abc8f745
Abdo Samir <abdo.samir.7719@gmail.com>
2023-08-18 18:25:06 +0300

    xx

--- interface/monthly-data-csv.js
+++ interface/monthly-data-csv.js
@@ -1,40 +1,66 @@
 /*{
-    caseReference:""
-    CaseStartDate:""
-    Claim ID:""
-    Unique Case ID:""
-    Case Concluded Date:""
-    Client 1 Forename:""
+    Type:  => MIAM or Med session
 
-    Client 1 Surname:""
-    Client 1 Date of Birth:""
-    Client 1 UCN:""
+    caseReference:""   => ref
+    CaseStartDate:""   => start date
 
-    Client 1 Postcode:""
-    Client 1 Gender:""
 
+    Claim ID:""  =>  ??📢      => number of miams per day
+    Unique Case ID:"" => ??📢   => case start date " mediatio date"  ,  that day per/+claims 
+
+    Case Concluded Date:"" => ??📢 => date of last mediation session record ?
+    Outcome => ??📢   =>    from mediationSessionRecord => mediationFinishReason a-b-c-s-p
+
+    Outreach Location  => ??📢 👌
+    Referral => ??📢 👌
+
+    VAT Indicator => ??📢                          = > Always "Y" (Yes
+    Net Disbursement Amount (    VAT) =>??📢       => undone 
+
+    Disbursements VAT amount => ??📢      => will be empty
+
+
+    Client 1 Postal Application accepted  => ??📢   => : Always "N" (NO
+    Client 2 Postal Application accepted => ??📢     => : Always "N" (NO
+
+
+    Schedule Reference (Outcome) => ??📢   =>   2P664C/MEDI2018/05 =<🤦‍♂️   06 will be changed in => in Sep 2023 change per year
+    
+    
+    Client 1 UCN:"" => ??📢  =>   DDMMYYYY/N/SURN   for birthdate
+                                 date of birth (without forward slash)/First letter of the name/four letter of their surname.
+    Client 2 UCN  => => ??📢    same
+
+
+    Client 1 Forename:"" =>fname
+    Client 1 Surname:"" =>sname
+    Client 1 Date of Birth:"" =>db
+    Client 1 Postcode:"" => ??🤦‍♂️ for MIAM 1
+    Client 1 Gender:"" => 
     Client 1 Ethnicity:""
-    Client 1 Disability:""
-    Client 1 Legally Aided?
+    Client 1 Disability:"" => ??🤦‍♂️ for MIAM 1
+    Client 1 Legally Aided?  => yes or no
 
     Client 2 Forename
     Client 2 Surname
     Client 2 Date of Birth
-    Client 2 UCN
     Client 2 Postcode
     Client 2 Gender
     Client 2 Ethnicity
     Client 2 Disability
     Client 2 Legally Aided?
-    Number of Mediation Sessions <
-    Mediation Time (mins) <
-    Outcome
-    Outreach Location
-    Referral
-    VAT Indicator
-    Net Disbursement Amount (    VAT)
-    Disbursements VAT amount
-    Client 1 Postal Application accepted 
-    Client 2 Postal Application accepted
-    Schedule Reference (Outcome)
+
+    Number of Mediation Sessions 
+    Mediation Time (mins) 
+
+
+
+
+    **********************
+    
+
+
+
+
+
 }*/
