a9f55b7e456e1555849586d2462665b331be7280
Lamiaa Hassan <mkabary8@gmail.com>
2023-08-31 16:30:32 +0300

    .

--- routes/auth.js
+++ routes/auth.js
@@ -67,7 +67,7 @@ router.post("/add-company", authMiddleware, async (req, res, next) => {
 
 
 
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     if (!passwordRegex.test(password)) {
       return res.status(400).json({ message: "Password must be at least 8 characters long and contain at least one letter and one number" });
     }
@@ -339,7 +339,7 @@ router.post('/add-mediator', authMiddleware, async (req, res, next) => {
       return res.status(400).json({ message: 'All fields are required!' });
     }
 
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
     // Check if the password meets the minimum requirements
     if (!passwordRegex.test(password)) {
@@ -439,7 +439,7 @@ router.post("/reset-password", async (req, res, next) => {
     const { resetToken, newPassword } = req.body;
 
 
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
     // Check if the password meets the minimum requirements
     if (!passwordRegex.test(newPassword)) {
